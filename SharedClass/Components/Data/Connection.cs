d51759419a7fd1a49d52b0aa8bc13be3e5f21098
DESKTOP-6JM2045\Dell <saadurrehman206@gmail.com>
2023-11-19 16:57:20 +0500

    temp

--- SharedClass/Components/Data/Connection.cs
+++ SharedClass/Components/Data/Connection.cs
@@ -4,7 +4,7 @@ namespace SharedClass.Components.Data
 {
     public class Connection
     {
-        private readonly string connectionString = "Server=192.168.100.1,1433;Data Source=DESKTOP-GVLTI9D\\SQLEXPRESS;Initial Catalog=Example;Encrypt=False;Integrated Security=True";
+        private readonly string connectionString = "Server=192.168.100.1,1433;Data Source=DESKTOP-6JM2045\\SQLEXPRESS;Initial Catalog=Example;Encrypt=false;Integrated Security=True;Trust Server Certificate=True";
 
         public SqlConnection GetSqlConnection()
         {
