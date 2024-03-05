15c78fafd7eda68beed92f15e4d19d890069a534
Ghazi Jaffri <ghazi.jaffri@hotmail.com>
2024-03-05 17:16:31 +0500

    Purchase Order

--- SharedClass/Components/Data/CRUD.cs
+++ SharedClass/Components/Data/CRUD.cs
@@ -1,30 +1,15 @@
 ﻿using Dapper;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Data.SqlClient;
-using Microsoft.IdentityModel.Protocols;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using SharedClass.Components.Data;
-using SharedClass.Components.Model;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
-using MudBlazor;
 
 namespace SharedClass.Components.Data
 {
-
     public class CRUD : Connection
     {
-
         public void CRD(dynamic Model, string SP, CommandType commandType = CommandType.StoredProcedure, bool IsDelete = false)
         {
-
             using (SqlConnection db = new SqlConnection(connectionString))
             {
-
                 if (IsDelete)
                 {
                     Model.ForInsert = 0;
@@ -35,13 +20,7 @@ namespace SharedClass.Components.Data
                 }
                 Model.CreationDate = DateTime.Now;
                 db.Execute(SP, (object)Model, commandType: commandType);
-
             }
-
-
-
         }
-
     }
 }
-
--- SharedClass/Components/Data/Connection.cs
+++ SharedClass/Components/Data/Connection.cs
@@ -9,51 +9,48 @@ namespace SharedClass.Components.Data
     {
         public readonly string connectionString;
 
-            public Connection()
+        public Connection()
         {
-            //string ipAddress = GetIPv4Addresses();
-            //connectionString = $"Data Source=192.168.2.107,1433;Initial Catalog=Computer;User Id=Admin;Password=12345;Encrypt=False;";
-            //connectionString = $"Data Source={ipAddress},1433;Initial Catalog=Example;User Id=Ghazi;Password=fC5y2qRU;Encrypt=False;";
-            connectionString = $"Data Source=DESKTOP-M62686B\\SQLEXPRESS;Initial Catalog=Computer;User Id=Admin;Password=12345;Encrypt=False;";
-
+            string ipAddress = GetIPv4Addresses(); //Auto IPv4
+            connectionString = $"Data Source={ipAddress},1433;Initial Catalog=Computer;User Id=Admin;Password=12345;Encrypt=False;"; //Auto IPv4
+            //connectionString = $"Data Source=192.168.2.107,1433;Initial Catalog=TechnoTrek;User Id=Admin;Password=12345;Encrypt=False;"; //Manual IPv4
+            //connectionString = $"Data Source=DESKTOP-M62686B\\SQLEXPRESS;Initial Catalog=Computer;User Id=Admin;Password=12345;Encrypt=False;"; //Sufiyan Local
         }
+
         public SqlConnection GetSqlConnection()
         {
             return new SqlConnection(connectionString);
         }
-        //private static string GetIPv4Addresses()
-        //{
-        //    string ipAddress = "";
-        //    // Get all network interfaces on the machine
-        //    NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
 
-        //    foreach (NetworkInterface networkInterface in networkInterfaces)
-        //    {
-        //        // Filter out loopback and non-operational interfaces
-        //        if (networkInterface.NetworkInterfaceType != NetworkInterfaceType.Loopback
-        //            && networkInterface.OperationalStatus == OperationalStatus.Up)
-        //        {
-        //            // Get the IP properties of the current interface
-        //            IPInterfaceProperties ipProperties = networkInterface.GetIPProperties();
+        private static string GetIPv4Addresses() //Auto IPv4
+        {
+            string ipAddress = "";
+            // Get all network interfaces on the machine
+            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
 
-        //            // Filter out IPv6 addresses and get the IPv4 addresses
-        //            IPAddress[] ipv4Addresses = ipProperties
-        //                .UnicastAddresses
-        //                .Where(addr => addr.Address.AddressFamily == AddressFamily.InterNetwork)
-        //                .Select(addr => addr.Address)
-        //                .ToArray();
+            foreach (NetworkInterface networkInterface in networkInterfaces)
+            {
+                // Filter out loopback and non-operational interfaces
+                if (networkInterface.NetworkInterfaceType != NetworkInterfaceType.Loopback
+                    && networkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    // Get the IP properties of the current interface
+                    IPInterfaceProperties ipProperties = networkInterface.GetIPProperties();
 
-        //            Console.WriteLine($"Interface: {networkInterface.Description}");
-        //            Console.WriteLine("IPv4 Addresses:");
+                    // Filter out IPv6 addresses and get the IPv4 addresses
+                    IPAddress[] ipv4Addresses = ipProperties
+                        .UnicastAddresses
+                        .Where(addr => addr.Address.AddressFamily == AddressFamily.InterNetwork)
+                        .Select(addr => addr.Address)
+                        .ToArray();
 
-        //            foreach (IPAddress ipv4Address in ipv4Addresses)
-        //            {
-        //                ipAddress = ipv4Address.ToString();
-        //            }
-        //        }
-
-        //    }
-        //    return ipAddress;
-        //}
+                    foreach (IPAddress ipv4Address in ipv4Addresses)
+                    {
+                        ipAddress = ipv4Address.ToString();
+                    }
+                }
+            }
+            return ipAddress;
+        }
     }
 }
