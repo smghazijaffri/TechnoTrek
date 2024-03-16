9435cc827620abdf269af67d929631ae696c316d
Sufiyan <m.sufiyanyousuf@gmail.com>
2024-03-16 17:06:57 +0500

    Commit for PR

---
+++ SharedClass/Components/Data/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedClass.Components.Data
+{
+    internal class Constants
+    {
+    }
+    public class PurhcaseStatuses 
+    {
+        public const string Pending = "Pending";
+        public const string Draft = "Draft";
+        public const string Cancelled = "Cancelled";
+    }
+}
