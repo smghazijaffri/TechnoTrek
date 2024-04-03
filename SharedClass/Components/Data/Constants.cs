dae3ab859d22a40b2a318307473e41b25f6f6793
Ghazi Jaffri <ghazi.jaffri@hotmail.com>
2024-04-04 02:49:06 +0500

    Statuses

--- SharedClass/Components/Data/Constants.cs
+++ SharedClass/Components/Data/Constants.cs
@@ -2,18 +2,19 @@
 {
     public class PurchaseStatuses
     {
-        public const string Pending = "Pending";
         public const string Draft = "Draft";
-        public const string Cancelled = "Cancelled";
-        public const string Submitted = "Submitted";
+        public const string Issued = "Issued";
+        public const string ToBill = "To Bill";
+        public const string Pending = "Pending";
         public const string Expired = "Expired";
         public const string Ordered = "Ordered";
-        public const string Issued = "Issued";
-        public const string Transferred = "Transferred";
         public const string Received = "Received";
-        public const string ToBill = "To Bill";
-        public const string ToReceive = "To Receive";
+        public const string Cancelled = "Cancelled";
+        public const string Submitted = "Submitted";
         public const string Completed = "Completed";
         public const string Delivered = "Delivered";
+        public const string ToReceive = "To Receive";
+        public const string Transferred = "Transferred";
+        public const string ToReceiveBill = "To Receive and Bill";
     }
 }
