61912c5c2fb98728d3347edc67d62b149fbd9108
smghazijaffri <smghazi14@gmail.com>
2024-05-28 21:06:22 +0500

    Delete Functionality

--- SharedClass/Components/Data/Delete.cs
+++ SharedClass/Components/Data/Delete.cs
@@ -1,6 +1,6 @@
 ﻿using Dapper;
-using Microsoft.Data.SqlClient;
 using Microsoft.JSInterop;
+using Microsoft.Data.SqlClient;
 
 namespace SharedClass.Components.Data
 {
@@ -31,5 +31,75 @@ namespace SharedClass.Components.Data
             }
         }
 
+        public async Task DeleteFromTable(string table1, string column, string id, string table2)
+        {
+            try
+            {
+                con.Close();
+
+                string query1 = $"DELETE FROM {table1} WHERE {column} = @Id";
+                string query2 = $"DELETE FROM {table2} WHERE {column} = @Id";
+
+                con.Open();
+
+                await con.ExecuteAsync(query2, new { Id = id });
+                await con.ExecuteAsync(query1, new { Id = id });
+                
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                con.Close();
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task DeleteFromTable(string table1, string column, string id)
+        {
+            try
+            {
+                con.Close();
+
+                string query1 = $"DELETE FROM {table1} WHERE {column} = @Id";
+
+                con.Open();
+
+                await con.ExecuteAsync(query1, new { Id = id });
+
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                con.Close();
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task DeleteFromTable(string table1, string column1, string column2, string id, string table2, string table3, string table4)
+        {
+            try
+            {
+                con.Close();
+
+                string query1 = $"DELETE FROM {table1} WHERE {column1} = @Id";
+                string query2 = $"DELETE FROM {table2} WHERE {column2} = @Id";
+                string query3 = $"DELETE FROM {table3} WHERE {column2} = @Id";
+                string query4 = $"DELETE FROM {table4} WHERE {column2} = @Id";
+
+                con.Open();
+
+                await con.ExecuteAsync(query4, new { Id = id });
+                await con.ExecuteAsync(query3, new { Id = id });
+                await con.ExecuteAsync(query2, new { Id = id });
+                await con.ExecuteAsync(query1, new { Id = id });
+
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                con.Close();
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
