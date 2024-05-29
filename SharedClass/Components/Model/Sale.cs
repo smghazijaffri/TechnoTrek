367d990757ceb4ef66f88614bcb8a6e360262ff3
smghazijaffri <smghazi14@gmail.com>
2024-05-29 19:08:15 +0500

    Sales Invoice Status Issue

--- SharedClass/Components/Model/Sale.cs
+++ SharedClass/Components/Model/Sale.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SharedClass.Components.Model
+﻿namespace SharedClass.Components.Model
 {
     public class SaleOrder : BaseRecord
     {
