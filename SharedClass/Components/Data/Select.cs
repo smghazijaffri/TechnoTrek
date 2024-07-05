444d246475c8b3593c276794b790316cf1a729f1
smghazijaffri <smghazi14@gmail.com>
2024-07-05 13:33:05 +0500

    Successful Fixing

--- SharedClass/Components/Data/Select.cs
+++ SharedClass/Components/Data/Select.cs
@@ -248,16 +248,13 @@ namespace SharedClass.Components.Data
 
         public static string HashPassword(string password)
         {
-            using (SHA256 sha256Hash = SHA256.Create())
+            byte[] bytes = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+            StringBuilder builder = new();
+            foreach (byte b in bytes)
             {
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
-                StringBuilder builder = new StringBuilder();
-                foreach (byte b in bytes)
-                {
-                    builder.Append(b.ToString("x2"));
-                }
-                return builder.ToString();
+                builder.Append(b.ToString("x2"));
             }
+            return "SHA256$" + builder.ToString();
         }
     }
 }
